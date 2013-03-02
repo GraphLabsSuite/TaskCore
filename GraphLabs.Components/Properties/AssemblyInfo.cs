cd75ef401841eaa1ad3dc6dbcec6616271e6d9b8
Святослав Захряпин <svt.zhr@gmail.com>
2013-03-02 20:50:57 +0000

    bug 0 Версия 1.0.0.0
    git-svn-id: https://svtz.ru/svn/graphlabs.core/trunk@5 f9484f95-dd45-4966-b92e-e894b3dcb0d8

--- GraphLabs.Components/Properties/AssemblyInfo.cs
+++ GraphLabs.Components/Properties/AssemblyInfo.cs
@@ -5,14 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphLabs.Tasks.Visualization")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphLabs.Tasks.Visualization")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("GraphLabs.Visualization")]
+[assembly: AssemblyCompany("National Research Nuclear University MEPhI")]
+[assembly: AssemblyProduct("GraphLabs")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +26,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- GraphLabs.Core/Properties/AssemblyInfo.cs
+++ GraphLabs.Core/Properties/AssemblyInfo.cs
@@ -6,14 +6,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphLabs.Tasks.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphLabs.Tasks.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("GraphLabs.Core")]
+[assembly: AssemblyCompany("National Research Nuclear University MEPhI")]
+[assembly: AssemblyProduct("GraphLabs")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
@@ -26,5 +21,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
