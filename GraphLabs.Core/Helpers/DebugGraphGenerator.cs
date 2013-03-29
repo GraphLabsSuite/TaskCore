04a24bdad308a63fb500013ee0719a1690dc0aaa
Анна Мащенко <amashchenko@f9484f95-dd45-4966-b92e-e894b3dcb0d8>
2013-03-29 08:40:41 +0000

    bug 0 Новый входной граф
    git-svn-id: https://svtz.ru/svn/graphlabs.core/trunk@16 f9484f95-dd45-4966-b92e-e894b3dcb0d8

--- GraphLabs.Core/Helpers/DebugGraphGenerator.cs
+++ GraphLabs.Core/Helpers/DebugGraphGenerator.cs
@@ -9,15 +9,17 @@ namespace GraphLabs.Core.Helpers
         /// <summary>asdsf </summary>
         public static byte[] GetSerializedGraph()
         {
-            var graph = DirectedGraph.CreateEmpty(10);
-            graph.AddEdge(new DirectedEdge(graph.Vertices[0], graph.Vertices[1]));
-            graph.AddEdge(new DirectedEdge(graph.Vertices[1], graph.Vertices[2]));
-            graph.AddEdge(new DirectedEdge(graph.Vertices[2], graph.Vertices[3]));
+            var graph = DirectedGraph.CreateEmpty(7);
+            graph.AddEdge(new DirectedEdge(graph.Vertices[0], graph.Vertices[5]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[1], graph.Vertices[0]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[1], graph.Vertices[5]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[2], graph.Vertices[1]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[2], graph.Vertices[5]));
             graph.AddEdge(new DirectedEdge(graph.Vertices[3], graph.Vertices[4]));
-            graph.AddEdge(new DirectedEdge(graph.Vertices[4], graph.Vertices[0]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[4], graph.Vertices[2]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[4], graph.Vertices[3]));
             graph.AddEdge(new DirectedEdge(graph.Vertices[5], graph.Vertices[6]));
-            graph.AddEdge(new DirectedEdge(graph.Vertices[6], graph.Vertices[7]));
-            graph.AddEdge(new DirectedEdge(graph.Vertices[7], graph.Vertices[8]));
+            graph.AddEdge(new DirectedEdge(graph.Vertices[6], graph.Vertices[4]));
 
             return GraphSerializer.Serialize(graph);
         }
