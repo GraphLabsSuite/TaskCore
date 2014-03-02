25eef219906de3905b94342d2c7d3a6a88098749
Святослав Захряпин <svt.zhr@gmail.com>
2014-03-02 17:26:22 +0000

    bug 0 выкинул предположительно лишний INotifyPropertyChanged
    git-svn-id: https://svtz.ru/svn/graphlabs.core/trunk@58 f9484f95-dd45-4966-b92e-e894b3dcb0d8

--- GraphLabs.Common/LogEventViewModel.cs
+++ GraphLabs.Common/LogEventViewModel.cs
@@ -1,60 +1,19 @@
 ﻿using System;
-using System.ComponentModel;
-using GraphLabs.Utils;
 using JetBrains.Annotations;
 
 namespace GraphLabs.Common
 {
     /// <summary> Сообщение лога </summary>
-    public class LogEventViewModel : INotifyPropertyChanged
+    [CoverageExclude]
+    public class LogEventViewModel
     {
-        private string _message;
-        private DateTime _timeStamp;
-        private int _penalty;
-
         /// <summary> Сообщение </summary>
-        public string Message
-        {
-            get { return _message; }
-            set
-            {
-                _message = value;
-                OnPropertyChanged(ExpressionUtility.NameForMember(() => Message));
-            }
-        }
+        public string Message { get; set; }
 
         /// <summary> Временная отметка </summary>
-        public DateTime TimeStamp
-        {
-            get { return _timeStamp; }
-            set
-            {
-                _timeStamp = value;
-                OnPropertyChanged(ExpressionUtility.NameForMember(() => TimeStamp));
-            }
-        }
+        public DateTime TimeStamp { get; set; }
 
         /// <summary> Штрафные баллы </summary>
-        public int Penalty
-        {
-            get { return _penalty; }
-            set
-            {
-                _penalty = value;
-                OnPropertyChanged(ExpressionUtility.NameForMember(() => Penalty));
-            }
-        }
-
-        /// <summary> Occurs when a property value changes. </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary> Occurs when a property value changes. </summary>
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged(string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public int Penalty { get; set; }
     }
 }
