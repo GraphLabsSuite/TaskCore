1815fa9923fa7fd7a93a9e6411deb0ae77880a89
Святослав Захряпин <svt.zhr@gmail.com>
2014-03-03 20:07:46 +0000

    bug 0 методы для получения параметров запуска Silverlight-приложения
    git-svn-id: https://svtz.ru/svn/graphlabs.core/trunk@69 f9484f95-dd45-4966-b92e-e894b3dcb0d8

---
+++ GraphLabs.CommonUI/StartupHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows;
+
+namespace GraphLabs.CommonUI
+{
+    /// <summary> Вспомогательные методы для получения параметров задания </summary>
+    public static class StartupHelper
+    {
+        /// <summary> Имя параметра "ID задания" </summary>
+        public const string TaskIdParam = "task_id";
+
+        /// <summary> Имя параметра "GUID сессии" </summary>
+        public const string SessionGuidParam = "session_guid";
+
+        /// <summary> Получить ID задания </summary>
+        public static long GetTaskId(this StartupEventArgs args)
+        {
+            Contract.Requires(args != null);
+
+            long result;
+            if (args.InitParams.ContainsKey(TaskIdParam) &&
+                long.TryParse(args.InitParams[TaskIdParam], out result))
+            {
+                return result;
+            }
+
+            throw new Exception("Не удалось получить параметр модуля-задания.");
+        }
+
+        /// <summary> Получить GUID сессии </summary>
+        public static Guid GetSessionGuid(this StartupEventArgs args)
+        {
+            Contract.Requires(args != null);
+
+            Guid result;
+            if (args.InitParams.ContainsKey(SessionGuidParam) &&
+                Guid.TryParse(args.InitParams[SessionGuidParam], out result))
+            {
+                return result;
+            }
+
+            throw new Exception("Не удалось получить параметр модуля-задания.");
+        }
+    }
+}
