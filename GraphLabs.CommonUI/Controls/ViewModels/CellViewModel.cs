35c004335adf9037107da6e65b5ec90b29e6f6d7
Илья Сильченков <isilchenkov@f9484f95-dd45-4966-b92e-e894b3dcb0d8>
2014-10-25 06:22:19 +0000

    Add new class CellViewModel.cs for new Matrix UI.
    git-svn-id: https://svtz.ru/svn/graphlabs.core/trunk@104 f9484f95-dd45-4966-b92e-e894b3dcb0d8

---
+++ GraphLabs.CommonUI/Controls/ViewModels/CellViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace GraphLabs.CommonUI.Controls.ViewModels
+{
+    public class CellViewModel<T> : DependencyObject
+    {
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value", 
+            typeof (T), 
+            typeof (CellViewModel<T>), 
+            new PropertyMetadata(default(T))
+        );
+
+        public T Value
+        {
+            get { return (T) GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackProperty = DependencyProperty.Register(
+            "Background",
+            typeof (Brush),
+            typeof (CellViewModel<T>),
+            new PropertyMetadata(default(Brush))
+        );
+
+        public Brush Background
+        {
+            get { return (Brush) GetValue(BackProperty); }
+            set { SetValue(BackProperty, value); }
+        }
+    }
+}
