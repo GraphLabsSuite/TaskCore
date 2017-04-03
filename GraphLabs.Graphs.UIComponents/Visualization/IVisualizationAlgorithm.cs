d3cef1b3c3e9edcf3441d41019ca29b41728a374
avalsa <slstep6@ya.ru>
2017-04-03 21:10:15 +0300

    dsdf

---
+++ GraphLabs.Graphs.UIComponents/Visualization/IVisualizationAlgorithm.cs
@@ -0,0 +1,19 @@
+﻿namespace GraphLabs.Graphs.UIComponents.Visualization
+{
+    /// <summary>
+    /// можно сделать свою визуализацию
+    /// </summary>
+    public interface IVisualizationAlgorithm
+    {
+        /// <summary>
+        /// имя алгоритма визуализации
+        /// </summary>
+        /// <returns></returns>
+        string Name();
+
+        /// <summary>
+        /// то как мы выпоняем визуализацию
+        /// </summary>
+        void Visualize();
+    }
+}
