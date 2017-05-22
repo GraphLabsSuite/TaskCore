47876995da1f62be1f14b06349afc3d3548bf5d1
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2017-05-22 21:23:09 +0300

    самую малость причесал код принтера матриц

--- GraphLabs.CommonUI/Controls/ViewModels/Matrix/MatrixPrinter.cs
+++ GraphLabs.CommonUI/Controls/ViewModels/Matrix/MatrixPrinter.cs
@@ -1,7 +1,7 @@
 ﻿using GraphLabs.Utils;
-using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace GraphLabs.CommonUI.Controls.ViewModels.Matrix
 {
@@ -9,23 +9,23 @@ namespace GraphLabs.CommonUI.Controls.ViewModels.Matrix
     public class MatrixPrinter
     {
         /// <summary> Представляет матрицу в виде строки </summary>
-        public string MatrixToString(ObservableCollection<ViewModels.MatrixRowViewModel<string>> matrix)
+        public string MatrixToString(IEnumerable<ViewModels.MatrixRowViewModel<string>> matrix)
         {
             var rows = new List<string>();
             matrix.ForEach(row =>
-                rows.Add($"({string.Join(", ", row).Remove(0,1)})")
+                rows.Add($"({string.Join(", ", row.Skip(1))})")
             );
             return $"({string.Join("; ", rows)})";
         }
 
         /// <summary> Представляет матрицу в виде нескольких строк </summary>
-        public string MatrixToStrings(ObservableCollection<ViewModels.MatrixRowViewModel<string>> matrix)
+        public string MatrixToStrings(IEnumerable<ViewModels.MatrixRowViewModel<string>> matrix)
         {
             var rows = new List<string>();
             matrix.ForEach(row =>
-                rows.Add($"{string.Join(" ", row).Remove(0,1)}")
+                rows.Add($"{string.Join(" ", row.Skip(1))}")
             );
-            return $"{string.Join("\n", rows)}";
+            return $"{string.Join(Environment.NewLine, rows)}";
         }
         
     }
